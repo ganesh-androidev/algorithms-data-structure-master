e962da9cc88836f8cb35600588282d4c79750348
Ganesh Ramamoorthy <gramamoorthy@fb.com>
2022-02-18 02:00:32 -0800

    Adding Singly linkedlist insert, traverse, delete etcs

---
+++ linkedlist/SinglyLinkedList.java
@@ -0,0 +1,74 @@
+class SinglyLinkedList {
+   static Node head = null;
+   static Node tail = null;
+
+   public static class Node {
+    int data;
+    Node next;
+
+    Node (int d) {
+        this.data = d;
+        this.next = null;
+    }
+   }
+
+    // addNext will add the new node to the end of the list
+    public void addNext(int data) {
+        // Create New Node 
+        Node newNode = new Node(data);
+        
+        // check if its a first node element
+        if (head ==null) {  
+            head = newNode;
+            tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+    }
+
+    // 10 --> 20 --> 30 TO 1 --> 10 --> 20 --> 30
+    public void prepend( int data) {
+        Node newNode = new Node(data);
+
+        if (this.head != null) {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+    }
+
+    private static void printList() {
+        Node n = head;
+
+        while (n !=null ) {
+            System.out.print("[ " + n.data + " ]");
+            n = n.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String arg[]) {
+        SinglyLinkedList linkedList  = new SinglyLinkedList();
+        linkedList.addNext(10);
+        printList();
+        linkedList.addNext(20);
+        printList();
+        linkedList.addNext(30);
+        printList();
+        linkedList.prepend(1);
+        printList();
+        /*
+        SinglyLinkedList firstHead = new SinglyLinkedList();
+        firstHead.head = new Node(10);
+        
+        Node secondHead = new Node(20);
+        Node thirdHead = new Node(30);
+        
+        firstHead.head.tail = secondHead;
+        secondHead.tail = thirdHead;
+        printList();
+        */
+
+    }
+
+}
